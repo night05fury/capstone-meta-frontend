4e2d36464223979ec1cb3481a2371843d818c6f0
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-12 23:37:00 +0530

    Made Booking Form,Booking Page,Header, Navigation Bar and Footer

--- src/components/pages/Booking.js
+++ src/components/pages/Booking.js
@@ -2,3 +2,27 @@ import React from "react";
 import {useNavigate} from "react-router-dom";
 import RestaurantPic from "../assets/restaurant.jpg";
 import BookingForm from "../sections/reservePages/BookingForm";
+
+// 
+
+const Booking =()=>{
+    const naviagate=useNavigate();
+
+    return (
+        <main className="booking">
+            <h1>Booking Table</h1>
+            <section className="">
+                <img src={RestaurantPic} alt="Restaurant View" 
+                style={{width:"100%", height:"auto",borderRadius:"5px"}}/>
+                
+            </section>
+            {/* Booking Form Section  */}
+            <section>
+                {/* importing the Booking Form component Page from sections>reservePages>Booking Form */}
+                <BookingForm naviagate= {naviagate}/>
+
+            </section>
+        </main>
+    );
+}
+export default Booking;
