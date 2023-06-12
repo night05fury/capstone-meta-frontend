1ec12cca39f1828cfd559966923f674b8e76e39b
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-06-12 23:08:42 +0530

    Made Booking Form,Booking Page,Header, Navigation Bar and Footer

---
+++ src/components/pages/Booking.js
@@ -0,0 +1,4 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import RestaurantPic from "../assets/restaurant.jpg";
+import BookingForm from "../sections/reservePages/BookingForm";
